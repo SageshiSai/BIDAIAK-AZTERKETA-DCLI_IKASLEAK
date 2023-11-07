973b1a6550b3150b4cae461372afd8e52571be71
SageshiSai <saikonogameryt@gmail.com>
2023-11-07 12:21:19 +0100

    node for quokka

--- js/bidaiak_azterk.js
+++ js/bidaiak_azterk.js
@@ -1,64 +1,66 @@
-tarjetas=[["AMERICA","Machu Picchu Peru","IRUDIAK/machu picchu.jpg","10","2100" ],["ASIA","Vietnam","IRUDIAK/vietnam.jpg","1","2800"],["AFRICA","Cataratas Victoria","IRUDIAK/africa victoria.jpg","11","3000"],["EUROPA","Croacia","IRUDIAK/croacia.jpg","8","1200"],["OCEANIA","Australia","IRUDIAK/australia.jpg","2","3200"],["ASIA","Taj Mahal","IRUDIAK/tajmahal.jpg","4","2500"],["AFRICA","Piramides de Egipto","IRUDIAK/egipto.jpg","7","2100"],["EUROPA","Roma","IRUDIAK/roma.jpg","6","1000"],["AMERICA","Amazonas","IRUDIAK/ecuadoramazonia.jpg","0","2300"],["OCEANIA","Nueva Zelanda","IRUDIAK/new_zealand.jpg","11","3400"]]
-texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac bibendum purus. Cras gravida, nisl at egestas pharetra, sapien sapien varius tellus, imperdiet laoreet urna ante ac elit. Nullam ac libero non ante luctus egestas id a erat. Fusce id diam in ante blandit porttitor et sit amet dolor"
+let tarjetas=[["AMERICA","Machu Picchu Peru","IRUDIAK/machu picchu.jpg","10","2100" ],["ASIA","Vietnam","IRUDIAK/vietnam.jpg","1","2800"],["AFRICA","Cataratas Victoria","IRUDIAK/africa victoria.jpg","11","3000"],["EUROPA","Croacia","IRUDIAK/croacia.jpg","8","1200"],["OCEANIA","Australia","IRUDIAK/australia.jpg","2","3200"],["ASIA","Taj Mahal","IRUDIAK/tajmahal.jpg","4","2500"],["AFRICA","Piramides de Egipto","IRUDIAK/egipto.jpg","7","2100"],["EUROPA","Roma","IRUDIAK/roma.jpg","6","1000"],["AMERICA","Amazonas","IRUDIAK/ecuadoramazonia.jpg","0","2300"],["OCEANIA","Nueva Zelanda","IRUDIAK/new_zealand.jpg","11","3400"]]
+const texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac bibendum purus. Cras gravida, nisl at egestas pharetra, sapien sapien varius tellus, imperdiet laoreet urna ante ac elit. Nullam ac libero non ante luctus egestas id a erat. Fusce id diam in ante blandit porttitor et sit amet dolor"
 
+function crearCards(num_id){
+    const tarjeta_container = document.querySelector("#seccion");
+
+    var tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta");
+    
+    var irudia = document.createElement("div");
+    irudia.classList.add("irudia");
+    
+    var h3 = document.createElement("h3");
+    h3.textContent = tarjetas[num_id][1];
+    irudia.appendChild(h3);
+    
+    var img = document.createElement("img");
+    img.src = tarjetas[num_id][2];
+    irudia.appendChild(img);
+    
+    var textos = document.createElement("div");
+    textos.classList.add("texto");
+    
+    var p = document.createElement("p");
+    p.textContent = texto;
+    textos.appendChild(p);
+    
+    var input = document.createElement("input");
+    input.type = "hidden";
+    input.id = num_id;
+    
+    tarjeta.appendChild(irudia);
+    tarjeta.appendChild(textos);
+    tarjeta.appendChild(input);
+    
+    tarjeta_container.appendChild(tarjeta);
+}
 
 var getClickNav = document.querySelector( "#navHeader" );
 
 getClickNav.addEventListener( "click", function( event ){
+
     const tarjeta_container = document.querySelector( "#seccion" );
     tarjeta_container.innerHTML = "";
     var pais = event.target.textContent.trim();
-        for ( var i = 0; i <= tarjetas.length; i++ ) {
-            
-            if(pais == tarjetas[ i ][ 0 ]){
-                const tarjeta_container = document.querySelector( "#seccion" );
 
-                    var contenido_tarjeta = `
-                        <div class="tarjeta">
-                            <div class="irudia">
-                                <h3>${ tarjetas[i][1] }</h3>
-                                <img src="${ tarjetas[i][2] }">
-                            </div> 
-                            <div class="texto">
-                                <p>
-                                ${ texto }  
-                                </p>
-                            </div>
-                            <input type="hidden" id="${i}"/>
-                        <div>
-                    `;
+        for ( var i = 0; i < tarjetas.length; i++ ) {
 
-                    tarjeta_container.innerHTML += contenido_tarjeta;
-                    var tarjeta = document.querySelectorAll( ".tarjeta" );
+            switch (pais) {
 
+                case tarjetas[ i ][ 0 ]:
                     
-    
-            } else if( pais == "ALL" ){
-                for( let i = 0; i < tarjetas.length; i++ ){
+                crearCards(i);
+                    
+                    break;
 
-                    const tarjeta_container = document.querySelector( "#seccion" );
+                case "ALL":
 
-                        var contenido_tarjeta = `
-                            <div class="tarjeta">
-                                <div class="irudia">
-                                    <h3>${ tarjetas[ i ][ 1 ] }</h3>
-                                    <img src="${ tarjetas[ i ][ 2 ] }">
-                                </div> 
-                                <div class="texto">
-                                    <p>
-                                    ${ texto }  
-                                    </p>
-                                </div>
-                                <input type="hidden" id="${i}"/>
-                            <div>
-                        `;
-                        tarjeta_container.innerHTML += contenido_tarjeta;
+                crearCards(i);
 
-                        
-                }
-                
-    
+                    break;
             }
+
             var tarjeta = document.querySelectorAll( ".tarjeta" );
 
             tarjeta.forEach(function ( tarjetas ) {
@@ -77,25 +79,7 @@ document.addEventListener( "DOMContentLoaded", function() {
     
     for( let i = 0; i < tarjetas.length; i++ ){
 
-    const tarjeta_container = document.querySelector( "#seccion" );
-
-        var contenido_tarjeta = `
-            
-            <div class="tarjeta" href="#">
-                <div class="irudia">
-                    <h3>${ tarjetas[ i ][ 1 ] }</h3>
-                    <img src="${ tarjetas[ i ][ 2 ] }">
-                </div> 
-                <div class="texto">
-                    <p>
-                    ${ texto }  
-                    </p>
-                </div>
-                <input type="hidden" id="${i}"/>
-            <div>
-            
-        `;
-        tarjeta_container.innerHTML += contenido_tarjeta;
+        crearCards(i);
     }
     var tarjeta = document.querySelectorAll( ".tarjeta" );
 
@@ -109,12 +93,14 @@ document.addEventListener( "DOMContentLoaded", function() {
 
         
     })
-    //BOTTON POPUP
-    var boton = document.querySelector( "button" )
-    boton.addEventListener("click", function() {
-        window.open("BIDAIAK_AZTERK_2.html", "ventana1","width=1000,height=700");
-    })
 });
+
+
+//BOTTON POPUP
+var boton = document.querySelector( "button" )
+boton.addEventListener("click", function() {
+    window.open("BIDAIAK_AZTERK_2.html", "ventana1","width=1000,height=700");
+})
 
 
 
--- js/bidaiak_azterk_2.js
+++ js/bidaiak_azterk_2.js
@@ -1,10 +1,15 @@
-tarjetas=[["AMERICA","Machu Picchu Peru","IRUDIAK/machu picchu.jpg","10","2100" ],["ASIA","Vietnam","IRUDIAK/vietnam.jpg","1","2800"],["AFRICA","Cataratas Victoria","IRUDIAK/africa victoria.jpg","11","3000"],["EUROPA","Croacia","IRUDIAK/croacia.jpg","8","1200"],["OCEANIA","Australia","IRUDIAK/australia.jpg","2","3200"],["ASIA","Taj Mahal","IRUDIAK/tajmahal.jpg","4","2500"],["AFRICA","Piramides de Egipto","IRUDIAK/egipto.jpg","7","2100"],["EUROPA","Roma","IRUDIAK/roma.jpg","6","1000"],["AMERICA","Amazonas","IRUDIAK/ecuadoramazonia.jpg","0","2300"],["OCEANIA","Nueva Zelanda","IRUDIAK/new_zealand.jpg","11","3400"]];
-texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac bibendum purus. Cras gravida, nisl at egestas pharetra, sapien sapien varius tellus, imperdiet laoreet urna ante ac elit. Nullam ac libero non ante luctus egestas id a erat. Fusce id diam in ante blandit porttitor et sit amet dolor"
+let tarjetas=[["AMERICA","Machu Picchu Peru","IRUDIAK/machu picchu.jpg","10","2100" ],["ASIA","Vietnam","IRUDIAK/vietnam.jpg","1","2800"],["AFRICA","Cataratas Victoria","IRUDIAK/africa victoria.jpg","11","3000"],["EUROPA","Croacia","IRUDIAK/croacia.jpg","8","1200"],["OCEANIA","Australia","IRUDIAK/australia.jpg","2","3200"],["ASIA","Taj Mahal","IRUDIAK/tajmahal.jpg","4","2500"],["AFRICA","Piramides de Egipto","IRUDIAK/egipto.jpg","7","2100"],["EUROPA","Roma","IRUDIAK/roma.jpg","6","1000"],["AMERICA","Amazonas","IRUDIAK/ecuadoramazonia.jpg","0","2300"],["OCEANIA","Nueva Zelanda","IRUDIAK/new_zealand.jpg","11","3400"]];
+const texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac bibendum purus. Cras gravida, nisl at egestas pharetra, sapien sapien varius tellus, imperdiet laoreet urna ante ac elit. Nullam ac libero non ante luctus egestas id a erat. Fusce id diam in ante blandit porttitor et sit amet dolor"
+const meses= ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
+
+let p = tarjetas[0]
+
+let [Continente, Ciudad] = p
 
 const selectElement = document.getElementById('selectContinent');
 var option_container = "";
-selectElement.addEventListener('change', function(event) {
+selectElement.addEventListener('change', function() {
 
   var selectedOption = selectElement.options[selectElement.selectedIndex].value;
   var option_container = document.querySelector( "#selectTravel" );
@@ -12,43 +17,79 @@ selectElement.addEventListener('change', function(event) {
   option_container.innerHTML = "";
   tarjetas.forEach( tarjeta =>{
     
-     if(selectedOption == tarjeta[0]){
-            const option_container = document.querySelector( "#selectTravel" );
+     if( selectedOption == tarjeta[ 0 ] ){
 
-            var contenido_option = `
-            
-            <option>${ tarjeta[1] }</option>
-            
-            `;
-            option_container.innerHTML += contenido_option;
+      const option_container = document.querySelector( "#selectTravel" );
+
+      var option = document.createElement( "option" );
+      option.textContent = tarjeta[ 1 ];
+
+      option_container.appendChild( option );
      }
      
   });
 
 });
 
-var option_container = document.querySelector( "#selectTravel" );
-     option_container.addEventListener( "chance", function(){
-      var selectedOption = selectElement.options[ selectElement.selectedIndex ].value;
-       console.log(selectedOption);
-      if( selectedOption == tarjeta[1] ){
+   var option_container = document.querySelector( "#selectTravel" );
+
+   option_container.addEventListener( "change", function() {
+
+      var div_container = document.querySelector( "#divSeccion" );
+      div_container.innerHTML = "";
+                           
+      var selectedOption = option_container.options[ option_container.selectedIndex ].textContent;
+
+      tarjetas.forEach( tarjeta =>{
+         
+         if( selectedOption == tarjeta[ 1 ] ){
 
             const option_container = document.querySelector( "#divSeccion" );
+            
+            var irudia = document.createElement( "div" );
+            irudia.classList.add( "irudia" );
+            
+            var h3 = document.createElement( "h3" );
+            h3.textContent = tarjeta[ 1 ];
+            irudia.appendChild( h3 );
+            
+            var img = document.createElement( "img" );
+            img.src = tarjeta[ 2 ];
+            irudia.appendChild( img );
+            
+            var textos = document.createElement( "div" );
+            textos.classList.add( "texto" );
+            
+            var p = document.createElement( "p" );
+            p.textContent = texto;
+            textos.appendChild( p );
+            
+            option_container.appendChild( irudia );
+            option_container.appendChild( textos );
+            console.log(tarjeta[ 3 ]);
+            var temporada = document.querySelectorAll("#divSeccion");
+            console.log(temporada)
 
-            var contenido_option = `
-               <div class="irudia">
-                  <h3>${ tarjeta[1] }</h3>
-                  <img src="${ tarjeta[2] }">
-               </div> 
-               <div class="texto">
-                  <p>
-                     ${ texto }
-                  </p>
-               </div>
-             `;
-            option_container.innerHTML += contenido_option;
-      }
+            if(tarjeta[ 3 ] == 6 || tarjeta[ 3 ] == 7 || tarjeta[ 3 ] == 11){
+               
+               temporada.textContent = "High season";
+               temporada.style.display = "block";
+               temporada.style.backgroundColor = "#f1f275";
+
+            } else if(tarjeta[ 3 ] == 9 || tarjeta[ 3 ] == 10 || tarjeta[ 3 ] == 1) {
+
+               temporada.textContent = "Low season";
+               temporada.style.display = "block";
+               temporada.style.backgroundColor = "#7ff695";
+               
+            }
+            
+
+         }
+         
+      });
+   
+   });
     
-   })
 
 
